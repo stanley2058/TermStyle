e5eccb1d8235bf00b9ea8c03ce0311288b786558
stanley2058 <stanley2058@yahoo.com.tw>
2021-04-25 14:36:52 +0800

    update hsl algorithm

--- utils/utils.go
+++ utils/utils.go
@@ -2,45 +2,48 @@ package utils
 
 import "math"
 
+func hueToRgb(p float64, q float64, t float64) float64 {
+	if t < 0 {
+		t++
+	}
+	if t > 1 {
+		t--
+	}
+	if t < 1/6 {
+		return p + (q-p)*6*t
+	}
+	if t < 1/2 {
+		return q
+	}
+	if t < 2/3 {
+		return p + (q-p)*6*(2/3-t)
+	}
+	return p
+}
+
 // HslToRgb converted hsl to rgb (0<= h <360, 0<= s,l <=1)
-func HslToRgb(h float64, s float64, l float64) (r uint8, g uint8, b uint8) {
+func HslToRgb(h float64, s float64, l float64) (uint8, uint8, uint8) {
 	h = math.Mod(h, 360)
 	s = math.Min(math.Max(s, 0), 1)
 	l = math.Min(math.Max(l, 0), 1)
 
-	c := (1 - math.Abs(2*l-1)) * s
-	x := c * (1 - math.Abs(math.Mod(h/60, 2)-1))
-	m := l - c/2
+	var rf, gf, bf, p, q float64
 
-	var rp, gp, bp float64
-	if h < 60 {
-		rp = c
-		gp = x
-		bp = 0
-	} else if h < 120 {
-		rp = x
-		gp = c
-		bp = 0
-	} else if h < 180 {
-		rp = 0
-		gp = c
-		bp = x
-	} else if h < 240 {
-		rp = 0
-		gp = x
-		bp = c
-	} else if h < 300 {
-		rp = x
-		gp = 0
-		bp = c
-	} else if h < 360 {
-		rp = c
-		gp = 0
-		bp = x
+	if s == 0 {
+		rf = l
+		gf = l
+		bf = l
+	} else {
+		if l < 0.5 {
+			q = l * (1 + s)
+		} else {
+			q = l + s - l*s
+		}
+		p = 2*l - q
+
+		rf = hueToRgb(p, q, h+1/3)
+		gf = hueToRgb(p, q, h)
+		bf = hueToRgb(p, q, h-1/3)
 	}
-
-	r = uint8((rp + m) * 255)
-	g = uint8((gp + m) * 255)
-	b = uint8((bp + m) * 255)
-	return r, g, b
+	return uint8(rf * 255), uint8(gf * 255), uint8(bf * 255)
 }
